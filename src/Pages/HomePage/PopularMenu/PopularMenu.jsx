43a51836e67c253172069e59a53368a2a8df2312
Imran <webimran2021@gmail.com>
2023-11-19 00:08:05 +0600

    create and using custom hook

--- src/Pages/HomePage/PopularMenu/PopularMenu.jsx
+++ src/Pages/HomePage/PopularMenu/PopularMenu.jsx
@@ -1,39 +1,43 @@
-import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import MenuItemCard from "../../SharedPage/MenuItemCard";
+import useMenu from "../../../components/hooks/useMenu";
 
 const PopularMenu = () => {
-    // state declar
-    const [menu, setMenu] = useState([]);
+    // // state declar
+    // const [menu, setMenu] = useState([]);
 
-    // json data load
-    useEffect(() => {
-        fetch("menu.json").then((res) => {
-            res.json().then((data) => {
-                const popularItems = data.filter(item => item.category === 'popular');
-                setMenu(popularItems)
-            });
-        });
-    }, []);
+    // // json data load
+    // useEffect(() => {
+    //     fetch("menu.json").then((res) => {
+    //         res.json().then((data) => {
+    //             const popularItems = data.filter(item => item.category === 'popular');
+    //             setMenu(popularItems)
+    //         });
+    //     });
+    // }, []);
+
+    // 2nd way use custom hook
+    const [menu] = useMenu();
+    const popular = menu.filter(item=> item.category === "popular")
 
 
-    return (
-        <section className="mb-12">
-            <SectionTitle
-                subHeading={"---Check it out---"}
-                heading={"FROM OUR MENU"}
-            ></SectionTitle>
-             <div className="grid md:grid-cols-2 gap-10">
+return (
+    <section className="mb-12">
+        <SectionTitle
+            subHeading={"---Check it out---"}
+            heading={"FROM OUR MENU"}
+        ></SectionTitle>
+        <div className="grid md:grid-cols-2 gap-10">
             {
-               menu.map(item=><MenuItemCard key={item._id} item={item}></MenuItemCard>) 
+                popular.map(item => <MenuItemCard key={item._id} item={item}></MenuItemCard>)
             }
-            </div>
-            <div className="text-center">
+        </div>
+        <div className="text-center">
             <button className="btn btn-outline border-0 border-b-4 mt-4 ">View Full Menu</button>
-            </div>
-        </section>
-       
-    );
+        </div>
+    </section>
+
+);
 };
 
 export default PopularMenu;
