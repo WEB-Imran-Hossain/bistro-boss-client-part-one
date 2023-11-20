d31c500dcbe7df52b2c96db61b4939a028a22eeb
Imran <webimran2021@gmail.com>
2023-11-21 03:07:43 +0600

    adding database and now properly working

--- src/Pages/HomePage/Testimonial/Testimonial.jsx
+++ src/Pages/HomePage/Testimonial/Testimonial.jsx
@@ -15,7 +15,7 @@ const Testimonial = () => {
 
     // JSON data load
     useEffect(() => {
-        fetch("reviews.json")
+        fetch("http://localhost:5000/reviews")
             .then((res) => res.json())
             .then((data) => setReviews(data));
     });
--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -9,7 +9,7 @@ const FoodCard = ({item}) => {
                <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Add to Cart</button>
+                    <button className="btn btn-outline border-0 border-b=a">Add to Cart</button>
                 </div>
                </div>
             </div>
